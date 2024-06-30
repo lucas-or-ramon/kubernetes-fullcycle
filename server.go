6ee70a931d026e05be0a87aa12d91934788ec4f8
Lucas Ramon <lucas.or.ramon@gmail.com>
2024-06-30 12:17:29 -0300

    feat: volumes

--- server.go
+++ server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":9090", nil)
 }
 
@@ -15,4 +18,13 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	w.Write([]byte(fmt.Sprintf("Hello, %s! You are %s years old.", name, age)))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Family members: %s", data)))
 }
